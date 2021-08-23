c2b25a1f8ccbffb257e833455a087722e50066b3
Minho Lee <91aiden.lee@gmail.com>
2021-08-24 02:10:17 +0900

    7장 수정

--- 07/07-3/src/router/index.js
+++ 07/07-3/src/router/index.js
@@ -1,22 +1,25 @@
 import Vue from 'vue'
+import MainPage from '@/components/MainPage'
+import SubPage from '@/components/SubPage'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    component: MainPage
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/main',
+    name: 'MainPage',
+    component: MainPage
+  },
+  {
+    path: '/sub',
+    name: 'SubPage',
+    component: SubPage
   }
 ]
 
