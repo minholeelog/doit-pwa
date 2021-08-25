01f75a84a9c287aacdf64622181053b9f25ec2a6
Minho Lee <91aiden.lee@gmail.com>
2021-08-25 21:16:36 +0900

    8장 뷰 프레임워크로 PWA 만들기

---
+++ 08/pwa-vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid fill-height>
+        <v-row>
+          <v-col cols="12" class="text-center">
+            <h1 class="title white--text">반가워요!</h1>
+            <p class="caption">by VueJS</p>
+            <img src="./assets/hello-pwa.png" alt="pwa" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created() {
+    this.$vuetify.theme.dark = true
+  }
+}
+</script>
+
+<style></style>
